ca7d55a2c41f1873c3c914e59a6ab05f9f3dcf37
LiusDev <wolflavamc@gmail.com>
2023-12-20 22:33:59 +0700

    fix: max file size

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,7 +11,9 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  app.use(json({ limit: '20mb' }));
+  app.use(urlencoded({ extended: true, limit: '20mb' }));
   app.enableCors();
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
