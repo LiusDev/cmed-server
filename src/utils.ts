f4f055d5ac7ae8e49a4dd076643b381d9c1e4450
rezarria <votanamcb@outlook.com>
2024-03-28 08:12:42 +0700

    Refactor toWebp function and add areArraysDifferent function

--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,23 @@ export async function toWebpString(data: string) {
 
 export async function toWebp(data: string) {
     const imgData = data.substring(data.lastIndexOf(',') + 1);
-    const featuredImageBuffer = await sharp(Buffer.from(imgData, "base64")).webp().toBuffer();
+    const featuredImageBuffer = await sharp(Buffer.from(imgData, "base64"), { failOnError: false }).webp().toBuffer();
     return featuredImageBuffer;
+}
+
+export function areArraysDifferent(arr1: string[], arr2: string[]): boolean {
+    if (arr1.length !== arr2.length) {
+        return true;
+    }
+
+    const sortedArr1 = arr1.slice().sort();
+    const sortedArr2 = arr2.slice().sort();
+
+    for (let i = 0; i < sortedArr1.length; i++) {
+        if (sortedArr1[i] !== sortedArr2[i]) {
+            return true;
+        }
+    }
+
+    return false;
 }
